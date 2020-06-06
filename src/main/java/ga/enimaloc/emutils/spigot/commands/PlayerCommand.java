ecb72622c8d575a0e2c36154e970691d86304aa3
unknown <antoinesainty23102003@gmail.com>
2020-06-06 23:39:54 +0200

    separated item in menu by methods

--- src/main/java/ga/enimaloc/emutils/spigot/commands/PlayerCommand.java
+++ src/main/java/ga/enimaloc/emutils/spigot/commands/PlayerCommand.java
@@ -55,7 +55,33 @@ public class PlayerCommand implements CommandExecutor {
     }
 
     /**
-     * Display GUI
+     * Display GUI <br>
+     * a = helmet <br>
+     * b = main hand <br>
+     * c = chestplate <br>
+     * d = other hand <br>
+     * e = leggings <br>
+     * f = boots <br>
+     * g = general info <br>
+     * h = xp stats <br>
+     * i = life stats <br>
+     * j = mined blocks <br>
+     * k = commands <br>
+     * l =  <br>
+     * m =  <br>
+     * n =  <br>
+     * o =  <br>
+     * p =  <br>
+     * q =  <br>
+     * r =  <br>
+     * s =  <br>
+     * t =  <br>
+     * u =  <br>
+     * v =  <br>
+     * w =  <br>
+     * x =  <br>
+     * y =  <br>
+     * z =  <br>
      * @param player {@link Player} to open GUI
      * @param target {@link Player target player}
      */
@@ -64,7 +90,6 @@ public class PlayerCommand implements CommandExecutor {
         Lang lang = Lang.getLang(player);
 
         InventoryGui gui;
-        final InventoryGui[] minedBlock = new InventoryGui[1];
 
         // Gui setup
         gui = new InventoryGui(
@@ -80,114 +105,173 @@ public class PlayerCommand implements CommandExecutor {
                 }
         );
 
-        // Helmet slot
-        gui.addElement(new DynamicGuiElement('a', () -> new StaticGuiElement('a', target.getInventory().getHelmet())));
-        // Main hand slot
-        gui.addElement(new DynamicGuiElement('b', () -> new StaticGuiElement('b', target.getInventory().getItemInMainHand())));
-        // Chestplate slot
-        gui.addElement(new DynamicGuiElement('c', () -> new StaticGuiElement('c', target.getInventory().getChestplate())));
-        // Other hand slot
-        gui.addElement(new DynamicGuiElement('d', () -> new StaticGuiElement('d', target.getInventory().getItemInOffHand())));
-        // Leggings slot
-        gui.addElement(new DynamicGuiElement('e', () -> new StaticGuiElement('e', target.getInventory().getLeggings())));
-        // Boots slot
-        gui.addElement(new DynamicGuiElement('f', () -> new StaticGuiElement('f', target.getInventory().getBoots())));
+        gui.addElement(helmet('a', target));
+        gui.addElement(mainHand('b', target));
+        gui.addElement(chestplate('c', target));
+        gui.addElement(offHand('d', target));
+        gui.addElement(leggings('e', target));
+        gui.addElement(boots('f', target));
 
-        // General info
-        gui.addElement(new DynamicGuiElement('g', () ->
-                        new StaticGuiElement(
-                                'g', new ItemStack(Material.PAPER),
-                                lang.get("inventory.player_info.general_info"),
-                                lang.get("inventory.player_info.general_info.hostname", target.getAddress().getAddress().getHostAddress()),
-                                lang.get("inventory.player_info.general_info.locale", target.getLocale()),
-                                lang.get("inventory.player_info.general_info.first_played", StringUtils.getFormattedDate(target.getFirstPlayed())),
-                                lang.get("inventory.player_info.general_info.premium", lang.get(""+emTarget.isPremium())),
-                                lang.get("inventory.player_info.general_info.online", lang.get(""+target.isOnline())),
-                                lang.get("inventory.player_info.general_info.op", lang.get(""+target.isOp())),
-                                lang.get("inventory.player_info.general_info.banned", lang.get(""+target.isBanned()))
-                        )
+        gui.addElement(generalInfo('g', target, lang, emTarget));
+        gui.addElement(xpStats('h', target, lang));
+        gui.addElement(lifeStats('i', target, lang));
+
+        gui.addElement(minedBlock('j', target, lang, emTarget, player));
+        gui.addElement(commands('k', target));
+
+        gui.setFiller(new ItemStack(Material.GRAY_STAINED_GLASS_PANE)); // Set item for empty slot
+        gui.show(player); // Open gui to player
+
+        // Update gui
+        new BukkitRunnable(){
+            @Override
+            public void run(){
+                if (player.getOpenInventory().getTitle().equals(gui.getTitle()))
+                    gui.draw();
+//                else this.cancel();
+            }
+        }.runTaskTimer(EmUtils.getInstance(), 0L, 1L);
+    }
+
+    private GuiElement helmet(char slot, OfflinePlayer target) {
+        return new DynamicGuiElement(
+                slot,
+                () -> new StaticGuiElement(slot, target.getPlayer().getInventory().getHelmet())
+        );
+    }
+    private GuiElement mainHand(char slot, OfflinePlayer target) {
+        return new DynamicGuiElement(
+                slot,
+                () -> new StaticGuiElement(slot, target.getPlayer().getInventory().getItemInMainHand())
+        );
+    }
+    private GuiElement chestplate(char slot, OfflinePlayer target) {
+        return new DynamicGuiElement(
+                slot,
+                () -> new StaticGuiElement(slot, target.getPlayer().getInventory().getChestplate())
+        );
+    }
+    private GuiElement offHand(char slot, OfflinePlayer target) {
+        return new DynamicGuiElement(
+                slot,
+                () -> new StaticGuiElement(slot, target.getPlayer().getInventory().getItemInOffHand())
+        );
+    }
+    private GuiElement leggings(char slot, OfflinePlayer target) {
+        return new DynamicGuiElement(
+                slot,
+                () -> new StaticGuiElement(slot, target.getPlayer().getInventory().getLeggings())
+        );
+    }
+    private GuiElement boots(char slot, OfflinePlayer target) {
+        return new DynamicGuiElement(
+                slot,
+                () -> new StaticGuiElement(slot, target.getPlayer().getInventory().getBoots())
+        );
+    }
+
+    private GuiElement generalInfo(char slot, OfflinePlayer target, Lang lang, EmPlayer emTarget) {
+        return new DynamicGuiElement(slot, () ->
+                new StaticGuiElement(
+                        slot, new ItemStack(Material.PAPER),
+                        lang.get("inventory.player_info.general_info"),
+                        lang.get("inventory.player_info.general_info.hostname", target.getPlayer().getAddress().getAddress().getHostAddress()),
+                        lang.get("inventory.player_info.general_info.locale", target.getPlayer().getLocale()),
+                        lang.get("inventory.player_info.general_info.first_played", StringUtils.getFormattedDate(target.getFirstPlayed())),
+                        lang.get("inventory.player_info.general_info.premium", lang.get(""+emTarget.isPremium())),
+                        lang.get("inventory.player_info.general_info.online", lang.get(""+target.isOnline())),
+                        lang.get("inventory.player_info.general_info.op", lang.get(""+target.isOp())),
+                        lang.get("inventory.player_info.general_info.banned", lang.get(""+target.isBanned()))
                 )
         );
-
-        // XP Stats
-        gui.addElement(new DynamicGuiElement('h', () ->
-                        new StaticGuiElement(
-                                'h', new ItemStack(Material.EXPERIENCE_BOTTLE),
-                                lang.get("inventory.player_info.xp_stats"),
-                                lang.get("inventory.player_info.xp_stats.level", target.getLevel()),
-                                StringUtils.formatProgressBar(Math.round(target.getExp()*100), 18)+" "+Math.round(target.getExp()*100)+"%"
-                        )
+    }
+    private GuiElement xpStats(char slot, OfflinePlayer target, Lang lang) {
+        return new DynamicGuiElement(slot, () ->
+                new StaticGuiElement(
+                        slot, new ItemStack(Material.EXPERIENCE_BOTTLE),
+                        lang.get("inventory.player_info.xp_stats"),
+                        lang.get("inventory.player_info.xp_stats.level", target.getPlayer().getLevel()),
+                        StringUtils.formatProgressBar(Math.round(target.getPlayer().getExp()*100), 18)+" "+Math.round(target.getPlayer().getExp()*100)+"%"
                 )
         );
-
-        // Life stats
-        gui.addElement(new DynamicGuiElement('i', () ->
-                        new StaticGuiElement(
-                                'i', new ItemStack(Material.APPLE),
-                                lang.get("inventory.player_info.life_stats"),
-                                lang.get("inventory.player_info.life_stats.health", target.getHealth()/2+"/"+target.getHealthScale()/2),
-                                lang.get("inventory.player_info.life_stats.food", target.getFoodLevel()),
-                                lang.get("inventory.player_info.life_stats.saturation", target.getSaturation())
-                        )
+    }
+    private GuiElement lifeStats(char slot, OfflinePlayer target, Lang lang) {
+        return new DynamicGuiElement(slot, () ->
+                new StaticGuiElement(
+                        slot, new ItemStack(Material.APPLE),
+                        lang.get("inventory.player_info.life_stats"),
+                        lang.get("inventory.player_info.life_stats.health", target.getPlayer().getHealth()/2+"/"+target.getPlayer().getHealthScale()/2),
+                        lang.get("inventory.player_info.life_stats.food", target.getPlayer().getFoodLevel()),
+                        lang.get("inventory.player_info.life_stats.saturation", target.getPlayer().getSaturation())
                 )
         );
+    }
 
-        // Mined block
-        gui.addElement(new DynamicGuiElement('j', () ->
-                        new StaticGuiElement(
-                                'j', new ItemStack(Material.DIAMOND_PICKAXE),
-                                click -> {
-                                    minedBlock[0] = new InventoryGui(
-                                            EmUtils.getInstance(),
-                                            lang.get("inventory.mined_block", target.getDisplayName()),
-                                            new String[]{
-                                                    "aaaaaaaaa",
-                                                    "aaaaaaaaa",
-                                                    "aaaaaaaaa",
-                                                    "aaaaaaaaa",
-                                                    "aaaaaaaaa",
-                                                    "b       c",
-                                            }
-                                    );
-                                    List<GuiElement> elements = new ArrayList<>();
-                                    for (Material m : emTarget.getSortedMinedBlocks().keySet()) {
-                                        elements.add(
-                                                new DynamicGuiElement(
-                                                        'a',
-                                                        () -> {
-                                                            ItemStack item = new ItemStack(m);
-                                                            ItemMeta itemMeta = item.getItemMeta();
-                                                            itemMeta.setLore(
-                                                                    Collections.singletonList(
-                                                                            lang.get(
-                                                                                    "inventory.mined_block.count",
-                                                                                    emTarget.getMinedBlockCount(m)
-                                                                            )
-                                                                    )
-                                                            );
-                                                            item.setItemMeta(itemMeta);
-                                                            return new StaticGuiElement(
-                                                                    'a',
-                                                                    item
-                                                            );
-                                                        }
-                                                )
-                                        );
+    private GuiElement minedBlock(char slot, OfflinePlayer target, Lang lang, EmPlayer emTarget, Player player) {
+        return new DynamicGuiElement(slot, () ->
+                new StaticGuiElement(
+                        slot, new ItemStack(Material.DIAMOND_PICKAXE),
+                        click -> {
+                            InventoryGui minedBlock = new InventoryGui(
+                                    EmUtils.getInstance(),
+                                    lang.get("inventory.mined_block", target.getName()),
+                                    new String[]{
+                                            "aaaaaaaaa",
+                                            "aaaaaaaaa",
+                                            "aaaaaaaaa",
+                                            "aaaaaaaaa",
+                                            "aaaaaaaaa",
+                                            "b       c",
                                     }
-                                    minedBlock[0].addElement(new GuiElementGroup('a', elements.toArray(new GuiElement[0])));
-                                    minedBlock[0].show(player);
+                            );
+                            List<GuiElement> elements = new ArrayList<>();
+                            for (Material m : emTarget.getSortedMinedBlocks().keySet()) {
+                                elements.add(
+                                        new DynamicGuiElement(
+                                                'a',
+                                                () -> {
+                                                    ItemStack item = new ItemStack(m);
+                                                    ItemMeta itemMeta = item.getItemMeta();
+                                                    itemMeta.setLore(
+                                                            Collections.singletonList(
+                                                                    lang.get(
+                                                                            "inventory.mined_block.count",
+                                                                            emTarget.getMinedBlockCount(m)
+                                                                    )
+                                                            )
+                                                    );
+                                                    item.setItemMeta(itemMeta);
+                                                    return new StaticGuiElement(
+                                                            'a',
+                                                            item
+                                                    );
+                                                }
+                                        )
+                                );
+                            }
+                            minedBlock.addElement(new GuiElementGroup('a', elements.toArray(new GuiElement[0])));
+                            minedBlock.show(player);
 
-                                    return true;
-                                },
-                                lang.get("inventory.player_info.mined_block"),
-                                lang.get("inventory.player_info.mined_block.total", emTarget.getMinedBlocks().values().stream().mapToInt(Integer::intValue).sum())
-                        )
+                            // Update inventory
+                            new BukkitRunnable(){
+                                @Override
+                                public void run(){
+                                    if (player.getOpenInventory().getTitle().equals(minedBlock.getTitle()))
+                                        minedBlock.draw();
+                                    else this.cancel();
+                                }
+                            }.runTaskTimer(EmUtils.getInstance(), 0L, 1L);
+
+                            return true;
+                        },
+                        lang.get("inventory.player_info.mined_block"),
+                        lang.get("inventory.player_info.mined_block.total", emTarget.getMinedBlocks().values().stream().mapToInt(Integer::intValue).sum())
                 )
         );
-
-        // Commands
-        gui.addElement(new StaticGuiElement(
-                'k',
+    }
+    private GuiElement commands(char slot, OfflinePlayer target) {
+        return new StaticGuiElement(
+                slot,
                 new ItemStack(Material.BOOK),
                 click -> {
 //                    ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
@@ -207,20 +291,6 @@ public class PlayerCommand implements CommandExecutor {
                     return true;
                 },
                 ChatColor.DARK_RED+"Disabled !"
-        ));
-
-        gui.setFiller(new ItemStack(Material.GRAY_STAINED_GLASS_PANE)); // Set item for empty slot
-        gui.show(player); // Open gui to player
-
-        // Update gui
-        new BukkitRunnable(){
-            @Override
-            public void run(){
-                if (player.getOpenInventory().getTitle().equals(gui.getTitle()))
-                    gui.draw();
-                else if (minedBlock[0] != null && player.getOpenInventory().getTitle().equals(minedBlock[0].getTitle()))
-                    minedBlock[0].draw();
-            }
-        }.runTaskTimer(EmUtils.getInstance(), 0L, 1L);
+        );
     }
 }
